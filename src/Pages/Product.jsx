de738e0462f56499b2ce83e83f954b5d261bf30f
Hardik <hardikturkhiya011@gmail.com>
2025-12-01 19:06:09 +0530

    paragraph add

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -104,13 +104,10 @@ function Product() {
           <h3 className="re">Review(3)</h3>
         </div>
         <p className="book">
-          You can search for more templates on Google Search using keywords such
-          as "templatemo digital marketing", "templatemo one-page",
-          "templatemo gallery", etc.
+          You can search for more templates on Google Search using keywords such as "templatemo digital marketing", "templatemo one-page", "templatemo gallery", etc. Please tell your friends about our website. If you need a variety of HTML templates, you may visit Tooplate and Too CSS websites.
         </p>
         <p className="book2">
-          Coloring book air plant shabby chic, crucifix normcore raclette cred
-          swag artisan activated charcoal...
+         Coloring book air plant shabby chic, crucifix normcore raclette cred swag artisan activated charcoal. PBR&B fanny pack pok pok gentrify truffaut kitsch helvetica jean shorts edison bulb poutine next level humblebrag la croix adaptogen. Hashtag poke literally locavore, beard marfa kogi bruh artisan succulents seitan tonx waistcoat chambray taxidermy. Same cred meggings 3 wolf moon lomo irony cray hell of bitters asymmetrical gluten-free art party raw denim chillwave tousled try-hard succulents street art.
         </p>
       </div>
 
